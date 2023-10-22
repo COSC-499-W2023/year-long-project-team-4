f63231a8ff4849e457fc5345d8698977ae23a01f
DenisGauthier <52676747+DenisGauthier@users.noreply.github.com>
2023-10-22 15:23:59 -0700

    Removed sensitive data from config

--- app/database/dbConfig.py
+++ app/database/dbConfig.py
@@ -3,12 +3,12 @@ ssh_username = 'ubuntu'
 
 
 #Replace with where your .pem file location is
-key_Path = 'C:/Users/Gauth/COSC499/year-long-project-team-4/app/database/DenisG-HomePC.pem'
+key_Path = ''
 
 
 USER='admin'
 #Repalce with the password
-PASS='Cosc499AWS-Team4'
+PASS=''
 HOST='127.0.0.1'
 #Change to the db name
-MYDB='Team4db'
+MYDB=''

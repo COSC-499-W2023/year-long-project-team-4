9db224f4fd9f6310a6466c6ece97885cb46d6ab3
DenisGauthier <52676747+DenisGauthier@users.noreply.github.com>
2023-10-30 21:33:00 -0700

    Removed cred from dbconfig

--- app/dbConfig.py
+++ app/dbConfig.py
@@ -3,12 +3,12 @@ ssh_username = 'ubuntu'
 
 
 #Replace with where your .pem file location is
-key_Path = '/Users/lakshaykarnwal/Documents/GitHub/COSC 499/year-long-project-team-4/app/database/Team4KP.pem'
+key_Path = ''
 
 
 USER='admin'
 #Repalce with the password
-PASS='Cosc499AWS-Team4'
+PASS=''
 HOST='127.0.0.1'
 #Change to the db name
-MYDB='Team4db'
+MYDB=''

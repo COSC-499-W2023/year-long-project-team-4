6ee03a1865f93af942053101cde65399327bfbc3
DenisGauthier <gauthier.34@hotmail.com>
2024-01-14 15:35:40 -0800

    Fixed missing frame issue + returned video in same fps as input

--- app/faceBlurring/faceDetection.py
+++ app/faceBlurring/faceDetection.py
@@ -49,48 +49,44 @@ def blur_faces_opencv(frame, face_details):
 
 if __name__ == '__main__':
     start = time.time()
+    
     # Temp variable for testing - load local files for it
     video_path = "C:/Users/Gauth/COSC499/year-long-project-team-4/tests/testVideo.mp4"
     video_out_path = "C:/Users/Gauth/COSC499/year-long-project-team-4/tests/testBlur_video1.mp4"
     
-    frame_skip = 15
+    # Set up Vars for the frame skipping
+    frame_skip = 8
     frame_num = 0
     sampled_frame = None
     sampled_face_details =[]
+    
     # Start handling video - Load, and save the output
     cap = cv2.VideoCapture(video_path)
+    # Match FPS of input to output
+    input_fps = cap.get(cv2.CAP_PROP_FPS)
     fourcc = cv2.VideoWriter_fourcc(*"mp4v")
-    out = cv2.VideoWriter(video_out_path, fourcc, 30,(int(cap.get(3)), int(cap.get(4))))
+    out = cv2.VideoWriter(video_out_path, fourcc, input_fps,(int(cap.get(3)), int(cap.get(4))))
 
+    # Loop over frames of the video/Open the video & Loop over it
     while cap.isOpened():
         ret, frame = cap.read()
+        # EOF Break
         if not ret:
             break
-        # Send call to Rekon
+        
+        # Check for new frame to pull
         if frame_num % frame_skip == 0:
+            # Do the analysis on pulled frame
             sampled_face_details = detect_faces(frame)
             sampled_frame = frame.copy()
-        elif sampled_frame is not None:
+        # Use previous frame boundries for frames inbetween pulls
+        if sampled_frame is not None:
             processed_frame = blur_faces_opencv(frame,sampled_face_details)
             out.write(processed_frame)
-            cv2.imshow("Blurred Faces", processed_frame)
-
         frame_num += 1
         
-        
-        #face_details = detect_faces(frame)
-        
-        # Take boundries from Reko and give to OpenCv to blur 
-        #output_frame = blur_faces_opencv(frame,face_details)
-        #out.write(output_frame)
-        
-        # Currently to display the blurring is working 
-        #cv2.imshow("Blurred Faces", processed_frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-        
+    # Free memory  
     cap.release()
     out.release()
-    cv2.destroyAllWindows()
     end = time.time()
     print(f"TOTAL TIME FOR PROCESSING: {end - start} seconds \n")

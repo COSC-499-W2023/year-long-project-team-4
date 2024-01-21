a452d02de3416338979d37e568e4241c75e6273a
DenisGauthier <gauthier.34@hotmail.com>
2024-01-21 13:12:13 -0800

    added videoLoc to end to avoid duplicate issue

--- app/faceBlurring/faceDetection.py
+++ app/faceBlurring/faceDetection.py
@@ -48,7 +48,7 @@ def blur_faces_opencv(frame, face_details):
     #print(f"BLUR CALL TIME: {end-start} seconds\n")
     return image
 
-def integrate_audio(original_video, output_video, audio_path=os.path.dirname(__file__)+'/temp/audio.mp4'):
+def integrate_audio(original_video, output_video, videoLoc, audio_path=os.path.dirname(__file__)+'/temp/audio.mp4'):
     # Extract audio
     my_clip = VideoFileClip(original_video)
     my_clip.audio.write_audiofile(audio_path,codec='libmp3lame')
@@ -58,8 +58,7 @@ def integrate_audio(original_video, output_video, audio_path=os.path.dirname(__f
     videoclip = VideoFileClip(output_video)
     #videoclip = videoclip.set_audio(VideoFileClip(audio_path))
     videoclip.write_videofile(temp_location, codec='libx264', audio=audio_path, audio_codec='libmp4lame')
-
-    os.rename(temp_location, os.path.dirname(__file__)+'/temp/blurredVideo.mp4')
+    os.rename(temp_location, os.path.dirname(__file__)+'/temp/'+videoLoc)
     # Delete audio
     os.remove(audio_path)
 
@@ -122,8 +121,8 @@ def process_video():
     # Temp variable for testing - load local files for it
     video_path = "C:/Users/Gauth/COSC499/year-long-project-team-4/tests/AudioTestUpdated.mp4"
     video_out_path = "C:/Users/Gauth/COSC499/year-long-project-team-4/tests/TestAudioBlur.mp4" 
-    frame_skip = 3
-    
+    frame_skip = 5
+    video_name = "testBlur.mp4"
     # This code below is for flask I believe? maybe not need confirmation
         # if 'file' not in request.files:
         #     return 'No file found'
@@ -131,10 +130,11 @@ def process_video():
         # if file.filename == '':
         #     return "no file selected"
         # uploaded_path = 'temp/uploaded_video.mp4'
+        # video_name = file.filename
         # file.save(uploaded_path)
         # processed_path = 'temp/processed_video.mp4'
         # parallel_detect_faces(uploaded_path,frame_skip, processed_path)
-        # integrate_audio(uploaded_path,processed_path)
+        # integrate_audio(uploaded_path,processed_path,video_name)
         # return send_file("../temp/blurredVideo.mp4", as_attachment=True)
     
     # number of frames between frame samples 
@@ -143,7 +143,7 @@ def process_video():
     
     # Start handling video - Load, and save the output
     parallel_detect_faces(video_path,frame_skip,video_out_path)
-    integrate_audio(video_path, video_out_path)
+    integrate_audio(video_path, video_out_path,video_name)
     #end = time.time()
     #print(f"TOTAL TIME FOR PROCESSING: {end - start} seconds \n") #//printing the speed 
     

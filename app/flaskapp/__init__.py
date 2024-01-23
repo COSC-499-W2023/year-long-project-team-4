fb13dea846257bb4903f01ac1a2b7d211ddcbf1c
Beth <bethc@shaw.ca>
2024-01-22 17:55:18 -0800

    added functions to init

--- app/flaskapp/__init__.py
+++ app/flaskapp/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_cors import CORS
 
-from .bucket_interface import rsa_encrypt_aes256_key, rsa_decrypt_aes256_key, aes_encrypt_video, aes_decrypt_video
+from .bucket_interface import rsa_encrypt_aes256_key, rsa_decrypt_aes256_key, aes_encrypt_video, aes_decrypt_video, set_verificationcode
 
 bcrypt = Bcrypt()
 
--- tests/test_change_password.py
+++ tests/test_change_password.py
@@ -19,31 +19,31 @@ def app():
 def client(app):
     return app.test_client()
 
-def test_change_password_reencrypt():
+def test_change_password_reencrypt(client):
     assert database.resetTable(tableName="userprofile", testcase=True)
     post_object = {'username': 'testDeleteUser','email': 'testDelete@example.com', 'password': 'test_password', 'firstname': 'John', 'lastname': 'Doe'}
     response = json.loads(client.post('/auth/signup', data=post_object).data.decode('utf-8'))
     assert not 'error' in response
 
-    response = client.post('/change_password_reencrypt/new_password', data=post_object)
+    response = json.loads(client.post('/change_password_reencrypt/new_password', data=post_object))
     assert response.status_code == 200
     
     current_password = database.query_records(table_name='userprofile', fields='password_hash', condition=f'username = %s', condition_values=('testDeleteUser',))
     assert current_password == 'new_password'
 
-def test_set_verificationcode():
+def test_set_verificationcode(client):
     post_object = {'username': 'fakeusertest987','email': 'fakeusertest987@gmail.com', 'password': 'test_password', 'firstname': 'John', 'lastname': 'Doe'}
-    response = client.post('/set_verificationcode/fakeusertest987@gmail.com', data=post_object)
+    response = json.loads(client.post('/set_verificationcode/fakeusertest987@gmail.com', data=post_object))
     assert response.status_code == 200 
     
-def test_change_password_forgot():
+def test_change_password_forgot(client):
     assert database.resetTable(tableName="userprofile", testcase=True)
     post_object = {'username': 'testDeleteUser','email': 'fakeusertest987@gmail.com', 'password': 'test_password', 'firstname': 'John', 'lastname': 'Doe'}
     response = json.loads(client.post('/auth/signup', data=post_object).data.decode('utf-8'))
     assert not 'error' in response
 
     input_code = flaskapp.set_verificationcode('fakeusertest987@gmail.com')
-    response = client.post('/change_password_forgot/fakeusertest987@gmail.com/new_password/'+ str(input_code))
+    response = json.loads(client.post('/change_password_forgot/fakeusertest987@gmail.com/new_password/'+ str(input_code)))
     assert response.status_code == 200
     
     current_password = database.query_records(table_name='userprofile', fields='password_hash', condition=f'username = %s', condition_values=('fakeusertest987',))

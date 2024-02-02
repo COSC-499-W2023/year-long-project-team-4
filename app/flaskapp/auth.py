0b85d298bfcff0cfc487f98afb1baff5a01b76fc
Beth <bethc@shaw.ca>
2024-02-01 18:04:05 -0800

    Add password specifications

--- app/flaskapp/auth.py
+++ app/flaskapp/auth.py
@@ -2,6 +2,7 @@ import os
 import boto3
 import random
 import string
+import re
 
 from flask import Blueprint, request, session, jsonify, current_app
 from rsa import generate_key
@@ -33,32 +34,26 @@ else:
 def signup():
     email = request.form.get('email')
     password = request.form.get('password')
-    firstname = request.form.get('firstname')
-    lastname = request.form.get('lastname')
     # Ensure request is valid
     if email is None:
         return jsonify({'error': 'Missing email'}), 400
     if password is None:
         return jsonify({'error': 'Missing password'}), 400
-    if password == '':
-        return jsonify({'error': 'Password must be non-empty'}), 400
-    if firstname is None:
-        return jsonify({'error': 'Missing first name'}), 400
-    if lastname is None:
-        return jsonify({'error': 'Missing last name'}), 400
+    specialCharacters = set('$#@!*')
+    if len(password) < 8:
+        return jsonify({"error": "Password less than 8 characters"}), 502
+    elif re.search('[0-9]',password) is None:
+        return jsonify({"error": "Password has no numbers"}), 502
+    elif re.search('[A-Z]',password) is None: 
+        return jsonify({"error": "Password has no capital letters"}), 502
+    elif re.search(specialCharacters,password) is None: 
+        return jsonify({"error": "Password has no special characters"}), 502
 
     # Ensure user doesn't already exist
     existing_user = database.query_records(table_name='userprofile', fields='email', condition=f'email = %s', condition_values=(email,))
     if existing_user:
         return jsonify({'error': 'User already exists'}), 409
 
-    salt_hash = os.urandom(64)
-    private_key_seed = password + salt_hash.hex()
-    private_key = generate_key(private_key_seed)
-    public_key = private_key.publickey().export_key('PEM')
-
-    hashed_password = bcrypt.generate_password_hash(password).decode()
-    
     #Create verification code
     created_code = ''.join(random.choices(string.digits, k=6))
     database.update_user(email,new_verifyKey = created_code)
@@ -69,6 +64,7 @@ def signup():
         file = open(completeName, "w")
         file.write(created_code)
         file.close()
+        return jsonify({"status": "success", "message": "code saved locally"}), 200
     #Send user an email with code
     try:
         response = ses_client.send_email(
@@ -79,20 +75,53 @@ def signup():
                 'Body': {'Text': {'Data': f'Your verification code is: {created_code}'}}
             }
         )
+        return jsonify({"status": "success", "message": "email sent"}), 200
     except Exception as e:
         print(f"Error sending email: {e}")
         return jsonify({"error": "Failed to send verification code via email"}), 502
 
-    result = database.insert_user(email=email, password=hashed_password, firstname=firstname, lastname=lastname, salthash=salt_hash, pubKey=public_key)
+@auth.route('/confirm_user', methods=['POST'])
+def confirm_user():
+    input_code = request.form.get('input_code')
+    email = request.form.get('email')
+    password = request.form.get('password')
+    firstname = request.form.get('firstname')
+    lastname = request.form.get('lastname')
+    # Ensure request is valid
+    if input_code is None:
+        return jsonify({'error': 'Missing input code'}), 400
+    if email is None:
+        return jsonify({'error': 'Missing email'}), 400
+    if password is None:
+        return jsonify({'error': 'Missing password'}), 400
+    if firstname is None:
+        return jsonify({'error': 'Missing first name'}), 400
+    if lastname is None:
+        return jsonify({'error': 'Missing last name'}), 400
     
-    if result == 1:
-        session['email'] = email
-        session['pkey_seed'] = password + salt_hash.hex()
-        return jsonify({'email': email}), 200
+    created_code = database.query_records(table_name='userprofile', fields='verifyKey', condition=f'email = %s', condition_values=(email,))[0]['verifyKey']
+    
+    if input_code == created_code:
+        salt_hash = os.urandom(64)
+        private_key_seed = password + salt_hash.hex()
+        private_key = generate_key(private_key_seed)
+        public_key = private_key.publickey().export_key('PEM')
+
+        hashed_password = bcrypt.generate_password_hash(password).decode()
+        
+        result = database.insert_user(email=email, password=hashed_password, firstname=firstname, lastname=lastname, salthash=salt_hash, pubKey=public_key)
+    
+        if result == 1:
+            session['email'] = email
+            session['pkey_seed'] = password + salt_hash.hex()
+            return jsonify({'email': email}), 200
+        else:
+            return jsonify({'error': 'Unknown error adding user'})
+    elif input_code != created_code:
+        return (jsonify({"status": "error", "message": "codes do not match"}),502)
     else:
-        return jsonify({'error': 'Unknown error adding user'})
-
-
+        return (jsonify({"status": "error", "message": "code not found"}), 502)
+    
 @auth.route('/login', methods=['POST'])
 def login():
     email = request.form.get('email')

b5570bbe1433c8f0e20f412e8c8fdff07f1cadc3
Lakshay Karnwal <60047109+lakshaykarnwal@users.noreply.github.com>
2024-03-04 18:38:12 -0800

    Update main.py
    removed debug statements

--- app/main.py
+++ app/main.py
@@ -4,5 +4,4 @@ app = create_app()
 
 if __name__ == '__main__':
     # specified 'port=8080' to avoid some errors on default port
-    #app.run(debug=True, port=8080)
     socketio.run(app, debug=True, port=8080)

bcce0e46acdeb651a456ec83bedad770a508a5e3
lakshaykarnwal <lakshaykarnwal@gmail.com>
2024-03-05 01:57:45 -0800

    Adjust Flask logging hot fix
    Adjust Flask logging to improve debugging

--- app/flaskapp/__init__.py
+++ app/flaskapp/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_cors import CORS
 from flask_socketio import SocketIO
+import logging
 
 bcrypt = Bcrypt()
 socketio = SocketIO()
@@ -10,6 +11,9 @@ socketio = SocketIO()
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'INSECURE_KEY_PLEASE_CHANGE_ME_BEFORE_ANYTHING_IMPORTANT'
+
+    # Adjust Flask logging to improve debugging
+    logging.getLogger('werkzeug').setLevel(logging.ERROR)
 
     # Configure CORS to handle cross-origin requests
     CORS(app, origins="http://localhost:3000", supports_credentials=True)

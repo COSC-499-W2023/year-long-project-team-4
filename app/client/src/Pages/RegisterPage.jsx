9998251a0f8336cd35a9c3856ee03d76bba6a64c
lakshaykarnwal <lakshaykarnwal@gmail.com>
2024-03-04 23:09:38 -0800

    completed email verification integration
    - user is prompted to enter email and verification code to complete the sign up

--- app/client/src/Pages/RegisterPage.jsx
+++ app/client/src/Pages/RegisterPage.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react'
-import { Button, Form, InputGroup} from 'react-bootstrap';
+import { Button, Form, InputGroup, Modal} from 'react-bootstrap';
 import {useNavigate} from 'react-router-dom'
 import {
     loginPath,
@@ -14,7 +14,30 @@ import unSee from '../Assets/eye-slash.svg';
 const RegisterPage = () => {
   const [type, setType] = useState(false);
   const [errorMessage, setErrorMessage] = useState(null);
+  const [modalErrorMessage, setModalErrorMessage] = useState(null);
+  const [showVerification, setShowVerification] = useState(false);
+  const [email, setEmail] = useState('');
+  const [verificationCode, setVerificationCode] = useState('');
   const navigate = useNavigate();
+
+  const handleVerification = async (verificationCode) => {
+    const formData = new URLSearchParams();
+    formData.append('email', email);
+    formData.append('input_code', verificationCode);
+
+    try {
+      const response = await axios.post(`${IP_ADDRESS}/auth/confirm_user`, formData);
+
+      if (response.data.status === 'success') {
+        console.log('Email verified');
+        navigate(loginPath); // navigate to login after successful verification.
+      } else {
+        setModalErrorMessage(response.data.message);
+      }
+    } catch (error) {
+      setModalErrorMessage('There was an error verifying the email');
+    }
+  };
 
 const handleSignup = async (firstname, lastname, email, password) => {
   const formData = new FormData();
@@ -22,20 +45,22 @@ const handleSignup = async (firstname, lastname, email, password) => {
   formData.append('lastname', lastname);
   formData.append('email', email);
   formData.append('password', password); // Add other form details as needed.
-  
-try {
-  const response = await axios({
-    method: 'post',
-    url: `${IP_ADDRESS}/auth/signup`,
-    data: formData,
-    headers: { 'Content-Type': 'multipart/form-data' }
-  });
 
+  
+  try {
+    const response = await axios({
+      method: 'post',
+      url: `${IP_ADDRESS}/auth/signup`,
+      data: formData,
+      headers: { 'Content-Type': 'multipart/form-data' }
+    });
+  
   const data = response.data;
 
-  if (data.email) {
+  if (email) {
+    setShowVerification(true); // Show verification input
     console.log('Signup successful');
-    navigate(loginPath); // navigate to login after successful registration.
+    //navigate(loginPath); // navigate to login after successful registration.
   } else {
     // Set the error message from the response
     setErrorMessage(data.error);
@@ -106,8 +131,42 @@ const handleSubmit = (e) => {
           </div>
         </Form.Group>  
         </Form>
-        <a className="text-primary"href={loginPath}>Have an account?</a>
+        <div><a className="text-primary" href="#!" onClick={() => setShowVerification(true)}>Have a Verification Code?</a></div>
+        <div><a className="text-primary" href={loginPath}>Have an account?</a></div>
       </Fade>
+      {/* Verification Modal */}
+      <Modal show={showVerification} onHide={() => setShowVerification(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>Verify Your Account</Modal.Title>
+        </Modal.Header>
+        {modalErrorMessage && <div className="alert alert-danger">{modalErrorMessage}</div>}
+        <Modal.Body>
+          <p>Please enter your email and the verification code sent there.</p>
+          <Form.Control
+            className="mb-3"
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <Form.Control
+            type="text"
+            placeholder="Verification Code"
+            onChange={(e) => setVerificationCode(e.target.value)}
+            required
+          />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => setShowVerification(false)}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={() => handleVerification(verificationCode)}
+          disabled={!email.trim() || !verificationCode.trim()}>
+            Verify
+          </Button>
+        </Modal.Footer>
+      </Modal>
   </div>
   )
 }

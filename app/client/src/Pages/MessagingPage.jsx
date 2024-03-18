a52319569f44f2b684f05a47c4a6d403107d7af8
Cole <colervs@gmail.com>
2024-03-17 17:44:54 -0700

    Stop video from attempting to reload itself, as the videoName state will never change while on the page

--- app/client/src/Pages/MessagingPage.jsx
+++ app/client/src/Pages/MessagingPage.jsx
@@ -81,7 +81,7 @@ function MessageSender() {
                 socket.disconnect();
             };
         }
-    }, [videoName]);
+    }, []);
 
     //Navigate back to view videos
     const handleBack = () => {
